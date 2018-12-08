87cb02f4a5c6b6e9abb7d6791d492197c7bc1346
brianMejia05 <brianariel04@yahoo.es>
2018-12-08 12:45:07 -0600

    implementando TextViews

--- app/src/androidTest/java/com/example/brianmejia/notas2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/brianmejia/notas2/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.brianmejia.notestwo
+package com.example.brianmejia.notas2
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.brianmejia.notestwo", appContext.packageName)
+        assertEquals("com.example.brianmejia.notas2", appContext.packageName)
     }
 }
